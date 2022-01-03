57702929ba9933aaea43a8bf8c2bbbcd846cc9a9
Janthanat Thanintantrakun <beamnut@hotmail.com>
2022-01-03 20:48:35 +0700

    feat: add calculator number

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+// feather icons for the symbols
 
 function App() {
   return (
     <div className="App">
       <div className="center">
-        hello
         <div className="center-divider"></div>
+        <div className="left-container">
+          <div className="left-element">
+            <div className="button-container">
+              <div className="button-symbol">
+                <button className="button">+</button>
+                <button className="button">-</button>
+                <button className="button">x</button>
+                <button className="button">/</button>
+              </div>
+              <div className="button-one-to-nine">
+                <button className="button">1</button>
+                <button className="button">2</button>
+                <button className="button">3</button>
+                <button className="button">4</button>
+                <button className="button">5</button>
+                <button className="button">6</button>
+                <button className="button">7</button>
+                <button className="button">8</button>
+                <button className="button">9</button>
+              </div>
+              <div className="button-zero-point">
+                <button className="button">0</button>
+                <button className="button">.</button>
+              </div>
+              <div className="button-ans-equal">
+                <button className="button">Ans</button>
+                <button className="button">=</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
