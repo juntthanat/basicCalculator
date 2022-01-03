fe21c1f9aa4cec50784b51a4924d055625b6290c
Janthanat Thanintantrakun <beamnut@hotmail.com>
2022-01-03 14:20:33 +0700

    feat: add background and structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="center">
+        hello
+        <div className="center-divider"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
